718b8b57ddd60b9d537c503978a31df5ce24b315
Mohd-nazim12 <sp.mn786@gmail.com>
2022-02-26 19:12:46 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
+import Home from "./Home";
+
 
  function App() {
   return (
-    <div>
-      
+  
+    <Home/>
+    
+  
 
-
-    </div>
   );
-}
-export default App;
+  };
+
+     export default App;
